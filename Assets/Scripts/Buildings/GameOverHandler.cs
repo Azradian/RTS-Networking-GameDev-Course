3635d656943355ca68a57c2b11124c3bd75d3ec7
Azradian <Azradian77@gmail.com>
2021-05-01 17:41:07 -0400

    Early implementation of new unit base and game over manager across network.

---
+++ Assets/Scripts/Buildings/GameOverHandler.cs
@@ -0,0 +1,47 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverHandler : NetworkBehaviour
+{
+    // List of bases the game keeps track of
+    private List<UnitBase> bases = new List<UnitBase>();
+
+    #region Server
+
+    public override void OnStartServer()
+    {
+        UnitBase.ServerOnBaseSpawned += ServerhandleBaseSpawned;
+        UnitBase.ServerOnBaseDespawned += ServerhandleBaseDespawned;
+    }
+
+    public override void OnStopServer()
+    {
+        UnitBase.ServerOnBaseSpawned -= ServerhandleBaseSpawned;
+        UnitBase.ServerOnBaseDespawned -= ServerhandleBaseDespawned;
+    }
+
+    [Server]
+    private void ServerhandleBaseSpawned(UnitBase unitBase)
+    {
+        bases.Add(unitBase);
+    }
+
+    [Server]
+    private void ServerhandleBaseDespawned(UnitBase unitBase)
+    {
+        bases.Remove(unitBase);
+
+        // Game continues as long as there's more than one player
+        if (bases.Count != 1)
+            return;
+
+        Debug.Log("Game Over!");
+    }
+
+    #endregion
+
+    #region Client
+    #endregion
+}
--- Assets/Scripts/Networking/RTSNetworkManager.cs
+++ Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using UnityEngine.SceneManagement;
 
 public class RTSNetworkManager : NetworkManager
 {
     [SerializeField] private GameObject unitSpawnerPrefab = null;
+    [SerializeField] private GameOverHandler gameOverHandlerPrefab = null;
 
     // Do this whenever a player is added to the server
     public override void OnServerAddPlayer(NetworkConnection conn)
@@ -19,4 +21,14 @@ public class RTSNetworkManager : NetworkManager
         NetworkServer.Spawn(unitSpawnerInstance, conn);
     }
 
+    public override void OnServerSceneChanged(string sceneName)
+    {
+        // Check if the scene we're going to is a playable map
+        if (SceneManager.GetActiveScene().name.StartsWith("Scene_Map"))
+        {
+            GameOverHandler gameOverHandlerInstance = Instantiate(gameOverHandlerPrefab);
+
+            NetworkServer.Spawn(gameOverHandlerInstance.gameObject);
+        }
+    }
 }
