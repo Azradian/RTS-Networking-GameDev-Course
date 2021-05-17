0b09141d491e26ea8617f2fa5b023333e6273800
Azradian <Azradian77@gmail.com>
2021-05-17 18:40:56 -0400

    Early implementation of Networking Lobby.

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void HostLobby()
+    {
+        NetworkManager.singleton.StartHost();
+    }
+}
